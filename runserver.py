e31fb84b8c55e330d4b8b90bd31cfa35a6fda603
dsisson <philosophe@gmail.com>
2018-03-19 18:40:34 -0700

    runserver.py: change port

--- runserver.py
+++ runserver.py
@@ -9,4 +9,4 @@ app.register_blueprint(api_blueprint, url_prefix='/api')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
