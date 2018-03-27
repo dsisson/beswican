b5b9ff7510b26cf064f47815c775799c150fdd5c
dsisson <philosophe@gmail.com>
2018-03-27 10:43:11 -0700

    runserver.py: update PORT specifier
    runserver.py
    + changed
       app.run(host='0.0.0.0', port=5001, debug=True)
      to
        app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))

--- runserver.py
+++ runserver.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 from beswican.api.phraser import api_blueprint
 from beswican.web.web import website_bp
 
@@ -9,4 +10,5 @@ app.register_blueprint(api_blueprint, url_prefix='/api')
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    #app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
